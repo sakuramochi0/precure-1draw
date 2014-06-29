cc92499e98e925cbf26b17d66ed6216fd4462982
sakuramochi0 <sakuramochi.mochi.0@gmail.com>
2014-06-29 11:46:18 +0900

    Fix generate_rank_html().

--- precure_1draw.py
+++ precure_1draw.py
@@ -1041,6 +1041,7 @@ def generate_rank_html():
             ax.set_ylabel('人数', fontproperties=fp)
             filename = '{}-{}.svg'.format(screen_name, date)
             plt.savefig(path.expanduser('~/www/') + filename)
+            plt.close()
          
             total = len(frs[date])
             rank = sorted(frs[date], reverse=True).index(fav)
@@ -1048,10 +1049,7 @@ def generate_rank_html():
          
             imgs.append('''<p style="margin-left: 4em;">{}{} - {}<br>Fav+RT: {}<br>Rank: {} / {} ({}%)</p>
             <img src="{tweet}" style="max-width: 500px;">
-            <img id="{src}" src="{src}">'''.format(get_labels_html(tweet, extra_class='user-label'), date, themes[date]['theme'],
-                                                   fav, rank, total, percent, src=filename,
-                                                   tweet='precure/1draw-collections/img/{}/{}'.format(tweet['tweet']['user']['id'],
-                                                                                               tweet['imgs'][0]['filename'])))
+            <img id="{src}" src="{src}">'''.format(get_labels_html(tweet, extra_class='user-label'), date, themes[date]['theme'], fav, rank, total, percent, src=filename, tweet='precure/1draw-collections/img/{}/{}'.format(tweet['tweet']['user']['id'], tweet['imgs'][0]['filename'])))
      
         with open('rank_template.html') as f:
             template = f.read()
