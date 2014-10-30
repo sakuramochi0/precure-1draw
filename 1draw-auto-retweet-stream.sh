70d7e8bf3ee360fd350d795feff7324349a39a44
sakuramochi0 <sakuramochi.mochi.0@gmail.com>
2014-10-30 15:58:44 +0900

    Fix for 1draw.

--- 1draw-auto-retweet-stream.sh
+++ 1draw-auto-retweet-stream.sh
@@ -1,5 +1,5 @@
 #!/bin/zsh
-pid=$(ps x | grep -i 1draw.py | grep -v ipython | grep -v grep | grep auto_retweet_stream | perl -pi -e 's/(\d+).+auto_retweet_stream.+/$1/')
+pid=$(ps x | grep -i 1draw.py | grep -v ipython | grep -v grep | grep auto_retweet_stream | perl -pi -e 's/.+?(\d+).+auto_retweet_stream.+?\n/$1/')
 time=$(date +%H%M)
 genre=$1; shift
 
@@ -9,5 +9,5 @@ if [ $pid ] && [ 2110 -gt $time ] && [ $time -gt 2100 ]; then
 fi
 # if not running, restart the program
 if [ ! $pid ]; then
-    1draw $genre 'auto_retweet_stream()' >> auto_retweet_stream-mongo.log
+    1draw $genre 'auto_retweet_stream()' >> $genre/auto_retweet_stream.log
 fi
